dff047e3695865f9fa9569e3f8afe59f701ae1c1
Tim Z <zhenghanecho@gmail.com>
2017-08-09 11:16:32 +0800

    Added MySQL Module

--- node/app/index.js
+++ node/app/index.js
@@ -1,16 +1,37 @@
-const express = require('express')  
-const app = express()  
-const port = 4444
+const express = require('express');
+const server = express();
+const port = 4444;
 
-app.get('/', (request, response) => {  
-  console.log(request.url)
-  response.send('Hello from Express!')
-})
+server.use((request, response, next) => {
+  request.chance = Math.random();
+  next();
+});
 
-app.listen(port, (err) => {  
+server.use((request, response, next) => {
+  console.log(request.headers);
+  next();
+});
+
+server.use((request, response, next) => {
+  response.status(404).json({
+      status: 404
+  });
+});
+
+server.get('/', (request, response) => {
+  console.log(request.url);
+  response.json({
+      chance: request.chance
+  });
+  // response.send('Hello from Express!')
+
+
+});
+
+server.listen(port, (err) => {
   if (err) {
-    return console.log('something bad happened', err)
+    return console.log('something bad happened', err);
   }
 
-  console.log(`server is listening on ${port}`)
-})
+  console.log(`server is listening on ${port}`);
+});
