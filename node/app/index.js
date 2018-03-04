00f94044e42e807d132a670db31136ea64c7f874
Tim Z <zhenghanecho@gmail.com>
2018-03-03 19:57:00 -0500

    Update

--- node/app/index.js
+++ node/app/index.js
@@ -2,15 +2,15 @@ const express = require('express');
 const server = express();
 const port = 4444;
 
-server.use((request, response, next) => {
-  request.chance = Math.random();
-  next();
-});
+// server.use((request, response, next) => {
+//   request.chance = Math.random();
+//   next();
+// });
 
-server.use((request, response, next) => {
-  console.log(request.headers);
-  next();
-});
+// server.use((request, response, next) => {
+//   console.log(request.headers);
+//   next();
+// });
 
 // server.use((request, response, next) => {
 //   response.status(200).json({
@@ -18,18 +18,18 @@ server.use((request, response, next) => {
 //   });
 // });
 
-server.get('/', (request, response) => {
+server.get('/random', (request, response) => {
   console.log(request.url);
   response.json({
-      chance: request.chance
+      chance: Math.random()
   });
   // response.send('Hello from Express!')
 });
 
 server.listen(port, (err) => {
   if (err) {
-    return console.log('something bad happened', err);
+    return console.log('Error: ', err);
   }
 
-  console.log(`server is listening on ${port}`);
+  console.log(`Server is listening on ${port}`);
 });
